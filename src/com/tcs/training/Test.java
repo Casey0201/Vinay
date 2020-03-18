3d9b6a3f9e657847094ded3c9e712a21aeeb83aa
TCS Interactive <tcsinteractive@TCSs-Mac-mini.local>
2020-03-18 13:04:33 +0530

    any message

---
+++ src/com/tcs/training/Test.java
@@ -0,0 +1,19 @@
+package com.tcs.training;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Test {
+
+ public static void main(String[] args) throws InterruptedException {
+	 WebDriver driver;
+	 System.out.println("a");
+	 System.setProperty("webdriver.gecko.driver","/Users/tcsinteractive/Documents/workspace-spring-tool-suite-4-4.5.0.RELEASE/Automation/geckodriver");
+	 driver=new FirefoxDriver();
+	 driver.get("https://www.google.com");
+	 Thread.sleep(5000);
+	 
+	 driver.quit();
+	}
+}
